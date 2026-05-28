4bb9c4f577a3ee07ae395cb55956c164b7b6f828
mahak <kaushikikes@gmail.com>
2026-05-28 22:36:26 +0530

    jwt middleware

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,3 +1,22 @@
 import jwt from "jsonwebtoken"
 import user from "../ models/userModel.js"
 
+export const protectRoute = async(req,res,next)=>{
+    try{
+        const token = req.cookies.jwt;
+
+        if (!token) {
+      return res.status(401).json({
+        message: "Unauthorized - No Token",
+      });
+
+    //   const decoded = jwt.verify(
+    //     token,
+    //     process.env.
+    //   )
+
+    }
+    }catch(err){
+
+    }
+}
